3374f0425e11708e6c9a9e2c7ada8e613f900eb9
Issam Alameh <issam@alameh.com>
2020-04-12 18:04:54 +1000

    After creating story route

--- lib/story.dart
+++ lib/story.dart
@@ -1,8 +1,8 @@
 // Step 2 - Create a new class called Story.
 class Story {
   String storyTitle;
-  String choice1;
-  String choice2;
+  List choice1;
+  List choice2;
   Story({this.storyTitle, this.choice1, this.choice2});
 }
 
